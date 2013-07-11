659b83bf5afe220497fab7777eebe68052a5a5b5
rruizriol <rembrandtruizriol@hotmail.com>
2013-07-11 11:09:18 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var readContent = function(file) {
+   var fs = require(’fs’);
+   return fs.readFile(file).toString();
+
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readContent('index.html'));
 });
 
 var port = process.env.PORT || 5000;
